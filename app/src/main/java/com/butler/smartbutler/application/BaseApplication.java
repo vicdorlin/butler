ae356c1ef1c2af0c05f475aa16a9d5e3e6ff83f7
linss <linss@aixuedai.com>
2018-12-12 16:26:00 +0800

    修改密码和忘记密码

--- app/src/main/java/com/butler/smartbutler/application/BaseApplication.java
+++ app/src/main/java/com/butler/smartbutler/application/BaseApplication.java
@@ -1,6 +1,7 @@
 package com.butler.smartbutler.application;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.butler.smartbutler.utils.StaticClass;
 import com.tencent.bugly.crashreport.CrashReport;
@@ -8,9 +9,13 @@ import com.tencent.bugly.crashreport.CrashReport;
 import cn.bmob.v3.Bmob;
 
 public class BaseApplication extends Application {
+    /**系统上下文*/
+    private static Context mAppContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mAppContext = getApplicationContext();
         initBugly();
         initBmob();
     }
@@ -40,5 +45,10 @@ public class BaseApplication extends Application {
      */
     private void initBugly() {
         CrashReport.initCrashReport(getApplicationContext(), StaticClass.BUGLY_APP_ID, true);
+    }
+
+    /**获取系统上下文：用于ToastUtil类*/
+    public static Context getAppContext(){
+        return mAppContext;
     }
 }
--- app/src/main/java/com/butler/smartbutler/ui/ForgetPasswordActivity.java
+++ app/src/main/java/com/butler/smartbutler/ui/ForgetPasswordActivity.java
@@ -1,10 +1,29 @@
 package com.butler.smartbutler.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.butler.smartbutler.R;
+import com.butler.smartbutler.utils.ShareUtils;
+import com.butler.smartbutler.utils.ToastUtil;
 
-public class ForgetPasswordActivity extends BaseActivity {
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
+
+public class ForgetPasswordActivity extends BaseActivity implements View.OnClickListener {
+    private EditText etNow;
+    private EditText etNew;
+    private EditText etConfirm;
+    private EditText etEmail;
+    private Button btnUpdatePwd;
+    private Button btnForgetPwd;
+    private TextView tvModPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +34,77 @@ public class ForgetPasswordActivity extends BaseActivity {
     }
 
     private void initView() {
+        tvModPwd = findViewById(R.id.tv_modPwd);
+        etNow = findViewById(R.id.et_now);
+        etNew = findViewById(R.id.et_new);
+        etConfirm = findViewById(R.id.et_new_confirm);
+        etEmail = findViewById(R.id.et_email);
+        btnUpdatePwd = findViewById(R.id.btn_update_password);
+        btnUpdatePwd.setOnClickListener(this);
+        btnForgetPwd = findViewById(R.id.btn_forget_password);
+        btnForgetPwd.setOnClickListener(this);
+        //用户未登录 隐藏修改密码框
+        if (!BmobUser.isLogin()) {
+            tvModPwd.setVisibility(View.GONE);
+            etNow.setVisibility(View.GONE);
+            etNew.setVisibility(View.GONE);
+            etConfirm.setVisibility(View.GONE);
+        }
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_update_password:
+                String oldPwd = etNow.getText().toString();
+                String newPwd = etNew.getText().toString();
+                String newPwdConfirm = etConfirm.getText().toString();
+                if (!newPwd.equals(newPwdConfirm)) {
+                    ToastUtil.showShortToast("确认密码不一致");
+                    break;
+                }
+                if (newPwd.equals(oldPwd)) {
+                    ToastUtil.showShortToast("新旧密码相同!");
+                    break;
+                }
+                //已经登录的用户才可以修改密码
+                BmobUser.updateCurrentUserPassword(oldPwd, newPwd, new UpdateListener() {
+                    @Override
+                    public void done(BmobException e) {
+                        if (e == null) {
+                            BmobUser.logOut();//退出登录
+                            ShareUtils.delShare(ForgetPasswordActivity.this, "password");
+                            startActivity(new Intent(ForgetPasswordActivity.this, LoginActivity.class));
+                            ToastUtil.showShortToast("密码更新成功，请重新登录");
+                            finish();
+                            return;
+                        } else {
+                            ToastUtil.showShortToast("更新失败");
+                            return;
+                        }
+                    }
+                });
+                break;
+            case R.id.btn_forget_password:
+                String email = etEmail.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    ToastUtil.showShortToast("请输入");
+                    return;
+                }
+                BmobUser.resetPasswordByEmail(email, new UpdateListener() {
+                    @Override
+                    public void done(BmobException e) {
+                        if (e == null) {
+                            ShareUtils.delShare(ForgetPasswordActivity.this, "password");
+                            startActivity(new Intent(ForgetPasswordActivity.this, LoginActivity.class));
+                            ToastUtil.showShortToast("邮件发送成功，请前往邮箱验证");
+                            finish();
+                        } else {
+                            ToastUtil.showShortToast("邮件发送失败 code:" + e.getErrorCode());
+                        }
+                    }
+                });
+                break;
+        }
     }
 }
